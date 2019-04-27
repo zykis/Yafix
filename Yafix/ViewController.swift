74d662e6ab035f92419241324ff8080b5a1f1ffa
Artem <zykis39@gmail.com>
2019-04-27 09:03:06 +0300

    Initial Commit

---
+++ Yafix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yafix
+//
+//  Created by Артём Зайцев on 27/04/2019.
+//  Copyright © 2019 Артём Зайцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
